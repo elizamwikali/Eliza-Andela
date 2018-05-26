a5d693e462fb64f5d2b8c3db303ab37819a80188
Your Name <you@example.com>
2018-05-26 13:32:03 +0300

    data structure

--- eliza.py
+++ eliza.py
@@ -7,3 +7,15 @@ print(first_list)
 first_list = ('eliza','maggy','ann')
 print(first_list)
 
+tuple1 = (1,2,3,4)
+print(tuple1)
+
+my_dict = {'Name': 'eliza', 'age': '40', 'course': 'media lab'}
+print(my_dict)
+
+laptop = 90
+if laptop > 80:
+    print("laptop is larger")
+else:
+    print("laptop is smaller")
+    
